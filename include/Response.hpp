cd80ba2903ed71be928c42f08cde7e0ac4ab2da3
Anastasia Nevodchikova <anevodchikova@MBP-Anastasia.sknt.ru>
2022-10-28 02:14:21 +0300

    begin response

---
+++ include/Response.hpp
@@ -0,0 +1,18 @@
+#include "Utils.hpp"
+
+class Response
+{
+private:
+	/* data */
+public:
+	Response(/* args */);
+	~Response();
+};
+
+Response::Response(/* args */)
+{
+}
+
+Response::~Response()
+{
+}
